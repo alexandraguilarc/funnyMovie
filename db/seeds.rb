d9c025b67471d93427c8e45f4507b2c84080e6c4
AlexandrAguilarC <alexandra10aguilarc@gmail.com>
2023-08-10 06:35:04 -0400

    Se agregan datos haciendo uso del seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating movies...'
+
+10.times do |i|
+    Movie.create(name: Faker::Movie.title, sypnosis: Faker::Movie.quote, director: Faker::Movie.name)
+end
+
+puts 'Creating series...'
+
+10.times do |i|
+    Serie.create(name: Faker::Movie.title, sypnosis: Faker::Movie.quote, director: Faker::Movie.name)
+end
+
+puts 'Creating documentaries...'
+
+10.times do |i|
+    DocumentaryFilm.create(name: Faker::Movie.title, sypnosis: Faker::Movie.quote, director: Faker::Movie.name)
+end
+
+
